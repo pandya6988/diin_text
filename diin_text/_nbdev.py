1cc779160dfe0dda64a38743bbc512d4033f7a09
pandya6988 <1potdish@gmail.com>
2021-12-26 02:09:09 +0000

    first

--- diin_text/_nbdev.py
+++ diin_text/_nbdev.py
@@ -5,7 +5,8 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 index = {"generate_translator": "00_data.ipynb",
          "default_translator_path": "00_data.ipynb",
          "strip_html": "00_data.ipynb",
-         "OriTraTranslation": "00_data.ipynb"}
+         "OriTraTranslation": "00_data.ipynb",
+         "create_ori_trans_dataframe": "00_data.ipynb"}
 
 modules = ["data.py"]
 
